c96a36527676b5b91f6fc4958f0b9d53136af244
Braden Elwell <be20@fsu.edu>
2023-09-11 11:37:08 -0400

    Initial Commit

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
   createCanvas(400, 400);
+  rectMode(CENTER);
+  noStroke();
 }
 
 function draw() {
-  background(125);
+  background("lightBlue");
+  
+  fill("red");
+  rect(200, 200, 100, 100);
+
+  fill("green");
+  ellipse(30, 30, 30, );
 }
